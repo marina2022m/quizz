f9bb4b86cdcbec82963844b1703c32e3c3db280b
marina2022m <106701554+marina2022m@users.noreply.github.com>
2023-09-28 14:03:01 -0300

    Create questions.js

---
+++ js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "A etica teleologica tradicional tem como fundamentação a ideia da finalidade?",
+    answer: "finalidade da ação",
+    options: [
+      "finalidade da ilusão",
+      "finalidade da imaginação",
+      "finalidade da falsidade",
+      "finalidade da ação"
+    ]
+  },
+  {
+    numb: 2,
+    question: "na filosofia cristã o telos e a?",
+    answer: "salvasão",
+    options: [
+      "mentira",
+      "ilusão",
+      "ações",
+      "salvasão"
+    ]
+  },
+  {
+    numb: 3,
+    question: "para o utilitarismo a finalidade das açoẽs humanas e o?",
+    answer: "prazer",
+    options: [
+      "prazer",
+      "inveja",
+      "falsidade",
+      "punição"
+    ]
+  },
+  {
+    numb: 4,
+    question: "A mentira não pode ser tomada como uma?",
+    answer: "lei",
+    options: [
+      "lei",
+      "vestigação",
+      "motivão",
+      "derterminar"
+    ]
+  },
+  {
+    numb: 5,
+    question: "A eudaimonia era o telos ou objetivo das ação?",
+    answer: "humanas",
+    options: [
+      "morte",
+      "humanas ",
+      "justiça",
+      "vitima"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
